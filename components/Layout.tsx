54ffd666b9e705e7213d86365cd8ec1fd788ca59
Hunter Chang <hunter@madwiremedia.com>
2021-04-08 21:06:34 -0600

    feat(theme): upgrade the theme toggle button

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { PostType } from '../types/post';
 import Navigation from './Navigation';
+import ThemeSwitch from './ThemeSwitch';
 
 export interface MetaProps
   extends Pick<PostType, 'date' | 'description' | 'image' | 'title'> {
@@ -65,38 +66,12 @@ const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
         <div className="max-w-5xl px-8 mx-auto">
           <div className="flex items-center justify-between py-6">
             <Navigation />
-            <button
-              aria-label="Toggle Dark Mode"
-              type="button"
-              className="bg-gray-200 dark:bg-gray-800 rounded p-3 h-10 w-10"
-              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-            >
-              {mounted && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  stroke="currentColor"
-                  className="h-4 w-4 text-gray-800 dark:text-gray-200"
-                >
-                  {theme === 'dark' ? (
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  ) : (
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                    />
-                  )}
-                </svg>
-              )}
-            </button>
+            {mounted && (
+              <ThemeSwitch
+                isDark={theme === 'dark'}
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+              />
+            )}
           </div>
         </div>
       </header>
@@ -105,7 +80,13 @@ const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
       </main>
       <footer className="py-8">
         <div className="max-w-5xl px-8 mx-auto">
-          Built by <a href="https://twitter.com/huntarosan">Hunter Chang</a>
+          Built by{' '}
+          <a
+            className="text-gray-900 dark:text-white"
+            href="https://twitter.com/huntarosan"
+          >
+            Hunter Chang
+          </a>
         </div>
       </footer>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,12 +15,19 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
     <Layout>
       <h1>Home Page</h1>
       <p>Next.js starter for your next blog or personal site. Built with:</p>
-      <ul className="list-disc pl-4">
+      <ul className="list-disc pl-4 my-6">
         <li>Next.js</li>
         <li className="mt-2">Typescript</li>
         <li className="mt-2">MDX</li>
         <li className="mt-2">Tailwind CSS</li>
       </ul>
+
+      <a
+        href="https://github.com/ChangoMan/nextjs-typescript-mdx-blog"
+        className="inline-block px-7 py-3 rounded-md text-white dark:text-white bg-blue-600 hover:bg-blue-700 hover:text-white dark:hover:text-white"
+      >
+        Get the source code!
+      </a>
 
       {posts.map((post) => (
         <article key={post.slug} className="mt-12">
