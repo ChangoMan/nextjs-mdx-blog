fe71d9a4de440716bec1e0c74f38be9a095951ec
Hunter Chang <hello@hunterchang.com>
2021-06-28 16:09:01 -0600

    chore(global): update nextjs and tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,7 @@
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
   plugins: {
-    // tailwindcss: {},
-    '@tailwindcss/jit': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const { spacing } = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
