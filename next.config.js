c4e7f08ce97c27ca63fb4cf56ab1188ee19198f5
Henry O'Donz <henrique.donzelli@gmail.com>
2022-01-20 01:22:32 +0100

    refactored next.config for env vars

--- next.config.js
+++ next.config.js
@@ -1,27 +1,30 @@
 const {
   PHASE_DEVELOPMENT_SERVER,
   PHASE_PRODUCTION_BUILD,
-} = require('next/constants')
+} = require('next/constants');
 
 // This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
 module.exports = (phase) => {
   // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
   // when `next build` or `npm run build` is used
-  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+  const isProd =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1';
   // when `next build` or `npm run build` is used
   const isStaging =
-    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1';
 
   // eslint-disable-next-line no-console
-  console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`)
+  console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`);
 
   const env = {
-    WEBSITE_HOST_URL : isProd ? 'http://monoplus.studio' : 'http://localhost:3000'
-  }
+    WEBSITE_HOST_URL: isProd
+      ? 'http://monoplus.studio'
+      : 'http://localhost:3000',
+  };
 
   // next.config.js object
   return {
     env,
-  }
-}
+  };
+};
